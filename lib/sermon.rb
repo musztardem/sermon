341bc449aa76ea86f6de34677e8aa751bc3c77b4
Antoni Pstraś <antek.pstras@gmail.com>
2017-05-30 20:32:01 +0200

    Added default config file and setup

--- lib/sermon.rb
+++ lib/sermon.rb
@@ -1,5 +1,24 @@
 require "sermon/version"
+require "fileutils"
 
 module Sermon
-  # Your code goes here...
+
+  CONFIG_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'config', 'sermon.yml'))
+  TARGET_PATH = File.expand_path('~/.sermon.yml')
+
+  def self.setup
+    FileUtils.cp(CONFIG_PATH, TARGET_PATH)
+
+    puts "Config file was copied to #{TARGET_PATH}."
+    puts 'Edit it accordingly to your needs.'
+  end
+
+  def self.check_for_config_file
+    unless Pathname.new(TARGET_PATH).exist?
+      puts "Config file not found."
+      puts "Run sermon --setup"
+      exit
+    end
+  end
+
 end
